56c33c1142d92a8a98baefdb142af85439c121ac
Hak Gyun Kim <gkrrbs1018@naver.com>
2021-12-24 10:45:12 +0900

    feat: how to deliver data using ListView Model when creating ListView

---
+++ App/ListViewSecondapp/app/src/main/java/org/techtown/listview_second_app/ListViewAdapter.kt
@@ -0,0 +1,40 @@
+package org.techtown.listview_second_app
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+class ListViewAdapter(val List: MutableList<ListViewModel>) : BaseAdapter(){
+    override fun getCount(): Int {
+        return List.size
+    }
+
+    override fun getItem(p0: Int): Any {
+        return List[p0]
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return p0.toLong()
+    }
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+
+        var convertView = p1
+
+        // List = [ListViewModel ("a", "b"), ListViewModel("c", "d"). e.d
+
+        if(convertView == null) {
+            convertView = LayoutInflater.from(p2?.context).inflate(R.layout.listview_item, p2, false)
+        }
+
+        val title = convertView!!.findViewById<TextView>(R.id.listviewItemText)
+        val title2 = convertView!!.findViewById<TextView>(R.id.listviewItemText2)
+        title.text = List[p0].text1
+        title2.text = List[p0].text2
+
+        return convertView!!
+    }
+
+}
---
+++ App/ListViewSecondapp/app/src/main/java/org/techtown/listview_second_app/ListViewModel.kt
@@ -0,0 +1,6 @@
+package org.techtown.listview_second_app
+
+data class ListViewModel (
+    val text1 : String,
+    val text2 : String
+)
