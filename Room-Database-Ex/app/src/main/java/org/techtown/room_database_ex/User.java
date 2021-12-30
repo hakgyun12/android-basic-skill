6b2bd9bb5a59957c8f763c74f0ca47daded99f0f
Hak Gyun Kim <gkrrbs1018@naver.com>
2021-12-30 10:18:35 +0900

    feat: Implementation of basic Room Database utilization methods.

---
+++ Room-Database-Ex/app/src/main/java/org/techtown/room_database_ex/User.java
@@ -0,0 +1,53 @@
+package org.techtown.room_database_ex;
+
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+// 객체라는 항목
+// 일반적인 클래스를 사용하는 것이 아닌 데이터 모델로써 사용하겠다는 부분이다.
+@Entity
+public class User {
+    @PrimaryKey(autoGenerate = true)
+    private int id = 0; // 하나의 사용자에 대한 고유 ID 값
+
+    private String name;
+
+    private String age;
+
+    private String phoneNumber;
+
+    // getter & setter 가져오거나 세팅을 하기 위한 준비단계
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
